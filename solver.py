7bd21d34ce34058c0172380eea9dff2930f344a1
Melvin Aristio <maristio@berkeley.edu>
2019-12-06 12:14:12 -0800

    update

--- solver.py
+++ solver.py
@@ -48,11 +48,63 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
     raw_pathway = tsp(startingIndex, clusters, distanceMatrix)
 
     # The result from TSP is a raw pathway, @Melvin please change this into the real pathway
-
+    real_pathway = realPath(raw_pathway, pathMatrix)
+    dropOffs = dropOff(raw_pathway, real_pathway, clusters, homes[:], distanceMatrix)
     # Once real pathway is done and we have the list of drop offs, map this back to the original name
     # with locationMap
 
+    # mapped_pathway = []
+    # for v in real_pathway:
+    # 	mapped_pathway.append(list_of_locations[v])
+
+    # mapped_dropOffs = []
+    # for i in dropOffs:
+    # 	drop = []
+    # 	for j in i:
+    # 		drop.append(list_of_locations[j])
+    # 	mapped_dropOffs.append(drop)
+
+    return real_path, dropOffs
+
     # Write to output file
+
+def realPath(tsp_path, all_paths):
+	real_path = [tsp_path[0]]
+	for i in range(len(tsp_path)-1):
+		real_path.extend(all_paths[tsp_path[i]][tsp_path[i+1]])
+	return real_path
+
+# centroids: tsp_path
+# clusters: dictionary of key = centroid and value is homes in the cluster
+# homes:  set of homes
+# distances: Matrix containing the distances
+def dropOff(tsp_path, real_path, clusters, homes, distances):
+	if tsp_path[0] in clusters:
+		centroids = tsp_path[:]
+	else:
+		centroids = tsp_path[1:]
+	drop = []
+
+	for v in real_path:
+		drop_location = []
+
+		if v in centroids:
+			for h in clusters[v]:
+				if h in homes:
+					drop_location.append(h)
+					homes.remove(h)
+			centroids.pop()
+		else:
+			for h in homes:
+				if distances[v][h] < distances[centroids[0]][h]:
+					drop_location.append(h)
+					homes.remove(h)
+
+		if drop_location:
+				drop.append([v] + drop_location)
+
+	return drop
+
 def shortestDistance(matrix):
 	distances = []
 	paths = []
